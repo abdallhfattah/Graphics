774520a1bd7eaa088c47ad72d664ffd2442029e3
abdallhfattah <abdallh.fattah33@gmail.com>
2024-05-09 22:41:10 +0300

    added unique line drawing

--- src/line/line.h
+++ src/line/line.h
@@ -6,6 +6,7 @@
 
 void DrawLineNaive(HDC hdc, int x1, int y1, int x2, int y2, COLORREF c);
 void DrawLineDDA(HDC hdc, int x1, int y1, int x2, int y2, COLORREF c);
-void DrawLineBresenham(HDC hdc, int x1, int y1, int x2, int y2, COLORREF color);
+void DrawLineBresenham(HDC hdc, int x1, int y1, int x2, int y2, COLORREF c);
+void UniqueLineDrawing(HDC hdc, int x1, int y1, int x2, int y2, COLORREF c);
 
-#endif 
+#endif
